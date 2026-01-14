b602460c4a1617247d26d6c4b7cc66cb90bdbe49
coders263@gmail.com <coders263@gmail.com>
2026-01-14 09:20:00 +0500

    import  methods

--- Packages/Course/course_details.py
+++ Packages/Course/course_details.py
@@ -2,7 +2,7 @@ import os,sys
 from os.path import dirname,abspath,join
 sys.path.insert(0,abspath(join(dirname(__file__),'..')))
 
-from payment import payment_details
+# from payment import payment_details
 
 def course_details():
     print("Its course details")
--- Packages/payment/payment_details.py
+++ Packages/payment/payment_details.py
@@ -1,2 +1,9 @@
+import os,sys
+from os.path import dirname,abspath,join
+sys.path.insert(0,abspath(join(dirname(__file__),'..')))
+from Course import course_details
+
 def payment():
     print("Its Payment method")
+
+course_details.course_details()
