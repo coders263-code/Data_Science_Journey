db7d642d975a3fd0d64d202d9e7433455ef21ffd
coders263@gmail.com <coders263@gmail.com>
2026-01-14 09:48:40 +0500

    modules and import Statements

--- Packages/details.py
+++ Packages/details.py
@@ -1,12 +1,14 @@
 # packages are diractory
 # Modules or inner files 
-# # syntex
+# # syntex 
+# from pack_name import file_name
+# link https://drive.google.com/file/d/1_b8_0kNRfUVjQclzmyw01e0Bq630DEq7/view
 """ This method is used to cover error due to paths
 import os,sys
 from os.path import dirname,abspath,join
 sys.path.insert(0,abspath(join(dirname(__file__),'..')))"""
 """
-from pack_name import file_name
+
 
 
 
